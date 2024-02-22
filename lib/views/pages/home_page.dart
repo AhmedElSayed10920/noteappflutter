627bd9a3fb6aaffc6c5439a7f73b035d47e36f2c
AhmedElSayed10920 <Kaido10920@gmail.com>
2024-02-22 15:37:03 +0200

    created customized app bar

---
+++ lib/views/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:noteapp/views/widgets/custom_ap_bar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 16),
+          child: Column(
+            children: [
+               CustomAppBar(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
