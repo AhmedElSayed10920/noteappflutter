2300fd22a29c3424ac6465f4dcb942d86011545c
AhmedElSayed10920 <Kaido10920@gmail.com>
2024-02-22 15:41:04 +0200

    refactor code 1

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:noteapp/views/widgets/custom_ap_bar.dart';
+import 'package:noteapp/views/widgets/custom_app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
