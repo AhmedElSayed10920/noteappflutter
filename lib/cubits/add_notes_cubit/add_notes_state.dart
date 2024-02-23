34196a7e855e4512688820508ece7af83f727274
AhmedElSayed10920 <Kaido10920@gmail.com>
2024-02-23 16:43:00 +0200

    add add notes state

---
+++ lib/cubits/add_notes_cubit/add_notes_state.dart
@@ -0,0 +1,14 @@
+part of 'add_notes_cubit.dart';
+
+abstract class AddNotesStates {}
+
+class AddNotesInitial extends AddNotesStates {}
+
+class AddNotesLoading extends AddNotesStates {}
+
+class AddNotesSuccess extends AddNotesStates {}
+
+class AddNotesFailure extends AddNotesStates {
+  final String errMessage;
+  AddNotesFailure({required this.errMessage});
+}
