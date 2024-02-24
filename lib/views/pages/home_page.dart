b581a0c826818231a202d07d8d9210d469f97a77
AhmedElSayed10920 <Kaido10920@gmail.com>
2024-02-24 15:44:48 +0200

    created fetch note method

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:noteapp/cubits/notes_cubit.dart/notes_cubit.dart';
 // import 'package:noteapp/views/pages/add_note.dart';
 import 'package:noteapp/views/widgets/add-notes-widgets/custom_add_note_bottom_sheet.dart';
 import 'package:noteapp/views/widgets/home-page-widgets/notes_view_body.dart';
@@ -9,28 +11,31 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Navigator.of(context).pushNamed(AddNote.id);
-          showModalBottomSheet(
-            isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16)),
-              context: context,
-              builder: (
-                context,
-              ) {
-                return const CustomAddNoteBottomSheet();
-              });
-        },
-        shape: const CircleBorder(),
-        child: const Icon(Icons.add),
-      ),
-      body: const SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          child: NotesViewBody(),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Navigator.of(context).pushNamed(AddNote.id);
+            showModalBottomSheet(
+                isScrollControlled: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                context: context,
+                builder: (
+                  context,
+                ) {
+                  return const CustomAddNoteBottomSheet();
+                });
+          },
+          shape: const CircleBorder(),
+          child: const Icon(Icons.add),
+        ),
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+            child: NotesViewBody(),
+          ),
         ),
       ),
     );
